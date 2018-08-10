998b73ceeda236dc0d5c80fd8d475dce9f233b8c
s.blinova <s.blinova@skbkontur.ru>
2018-08-10 10:51:02 +0300

    added some more plugins

--- removeOutdatedIEStyles.js
+++ removeOutdatedIEStyles.js
@@ -6,25 +6,48 @@ const removeStyles = function(file) {
     const discard = require('postcss-discard');
     const unprefix = require("postcss-unprefix");
     const autoprefixer = require("autoprefixer");
+    const stylelint = require("stylelint");
+    const stylefmt = require("stylefmt");
+    const prettify = require("postcss-prettify");
+    const stylehacks = require("stylehacks");
+    const border = require('postcss-border');
+    const merge = require('postcss-merge-longhand');
 
-    const plugins = [discard({
-        rule: [/^\s*\* ?html/,
-            /\.ie-(?!(11|any))/,
-            /\.ie[89]/,
-            /\..+-ie(?!-any)(?=,|\b|{)/,
-            /::-ms-clear/,
-            /::-ms-reveal/
-        ],
-        decl: [/^\s*zoom:/,
-            /.*\\9$/,
-            /.?.*\/\*\\\*\*\/ *{/,
-            /.+:.+\\0\//,
-            /\u002A/,
-            /filter/
-        ]
-    }), unprefix, autoprefixer];
+    const plugins = [
+        stylehacks,
+        prettify({
+            syntax: "less"
+        }),
+        merge,
+        discard({
+            rule: [/^\s*\* ?html/,
+                /\.ie-(?!(11|any))/,
+                /\.ie[89]/,
+                /\..+-ie(?!-any)(?=,|\b|{)/,
+                /\.body__1/,
+                /::-ms-clear/,
+                /::-ms-reveal/
+            ],
+            decl: [/^\s*zoom:/,
+                /.*\\9$/,
+                /.?.*\/\*\\\*\*\/ *{/,
+                /.+:.+\\0\//,
+                /filter/
+            ]
+        }),
+        unprefix,
+        autoprefixer,
+        stylefmt({
+            configFile: "../../diadoc/_Src/Hosting/web-app/.stylelintrc"
+        }),
+        stylelint({
+            configFile: "../../diadoc/_Src/Hosting/web-app/.stylelintrc",
+            fix: true
+        }),
+        border
+    ];
 
-    if ((/\.css$/i).test(file)) {
+    if ((/\.css$/i).test(file) && !(/\.min.css$/i).test(file)) {
         postcss(plugins)
             .process(fs.readFileSync(file).toString())
             .then(function (result) {
